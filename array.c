f02e39fc820beec7463eb32ef0cef94400dc8c0a
Gareema Kumari Gupta <gareema.k.gupta@gmail.com>
2026-04-13 14:09:14 +0530

    Create array.c

---
+++ array.c
@@ -0,0 +1,73 @@
+#include<stdio.h>
+int display(int *ptr,int size){
+	printf("\nupdated array\n");
+	for(int i=0;i<size;i++){
+		printf("%d\t",ptr[i]);
+	}
+	printf("\n");
+}
+
+int main(){
+	int arr[]={3,4,8,12},opt=0;
+
+printf("which operation would you like to do?\n1.Add element in beginning\n2.Add element in the end\n3. Insert a element in between\n choose an option: ");
+scanf("%d",&opt);
+	
+
+
+switch(opt){
+  case 3:	
+    
+    //to add a element in between
+    
+	  printf("Origional\n");
+	  int size=sizeof(arr)/sizeof(arr[0]);
+	  for(int i=0;i<size;i++){
+		  printf("%d\t",arr[i]);
+	  }
+	  printf("\n");
+	  int pos=1;
+  	for(int i=size;i>=pos;i--){
+		  if(i==1){
+			  arr[2]=6;
+		  }else{
+			  arr[i+1]=arr[i];
+		  }
+	  }
+	  size+=1;
+	  display(arr,size);
+	  printf("\n");
+
+    break;
+	
+	
+  case 1:
+    
+    //to add a element in the beginning of the array
+
+	  for(int i=size;i>=0;i--){
+		  arr[i+1]=arr[i];
+	  }
+	  arr[0]=10;
+	  size+=1;
+  	display(arr,size);
+
+    break;
+	
+
+  case 2:	
+    
+    //to add an element in the end
+
+	  arr[size]=64;
+	  size+=1;
+	  display(arr,size);
+
+    break;
+
+  default:
+    printf("Invalid option\n");
+
+    break;
+  }
+}
