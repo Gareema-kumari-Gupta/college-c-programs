3a20c4f1f9751a7edccc50932f567327f30a8474
Gareema Kumari Gupta <gareema.k.gupta@gmail.com>
2026-04-13 11:26:43 +0530

    Update op.c

--- op.c
+++ op.c
@@ -1,12 +1,21 @@
 #include<stdio.h>
 int main(){
-	int arr[4]={3,4,8,12},num=0,num1=0;
+	int arr[4]={3,4,8,12},num=0,num1=0,int myarr[6],int arrdup[5];
+
+	//to print all the elemnents in the array 
+	
 	for(int i=0;i<4;i++){
 		printf("%d\t",arr[i]);
 	}
+
+	// to print 3rd element
+
+	printf("3rd element of the array is %d\n",arr[2]);
+
+	// to add a element at the end of an array
 	printf("\nenter a number: ");
 	scanf("%d",&num);
-	int arrdup[5];
+	
 	for(int i=0; i<5;i++){
 		arrdup[i]=arr[i];
 	}
@@ -14,7 +23,9 @@ int main(){
 	for(int i=0;i<5;i++){
 		printf("%d\t",arrdup[i]);
 	}
-	int myarr[6];
+
+	//  to add a element in between
+	
 	printf("\nenter another number: ");
 	scanf("%d",&num1);
 	for(int i=0;i<6;i++){
@@ -26,6 +37,19 @@ int main(){
 	}
 	myarr[2]=num1;
 	for(int i=0; i<6;i++){
+		printf("%d\t",myarr[i]);
+	}
+	printf("\n");
+
+	// to add the element at the beginning
+
+	printf("\nenter a number: ");
+	scanf("%d",&num);
+	for(int i=0;i<4;i++){
+		myarr[i+1]=arr[i];
+	}
+	myarr[0]=num;
+	for(int i=0;i<5;i++){
 		printf("%d\t",myarr[i]);
 	}
 	printf("\n");
