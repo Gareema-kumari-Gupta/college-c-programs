58c96f88f61ed3d2e3c775f85cfbb4e3212210ab
Gareema Kumari Gupta <gareema.k.gupta@gmail.com>
2026-04-14 11:28:57 +0530

    Update array.c

--- array.c
+++ array.c
@@ -1,75 +1,98 @@
 #include<stdio.h>
-int display(int *ptr,int size){
-	printf("\nupdated array\n");
-	for(int i=0;i<size;i++){
+
+int origional(int *ptr,int size){
+
+	printf("Origional\n");
+	for(int i=0;i<size-1;i++){
 		printf("%d\t",ptr[i]);
 	}
 	printf("\n");
 }
 
+int display(int *ptr,int size){
+	
+	printf("\nupdated array\n");
+	for(int i=0;i<size;i++){
+		printf("%d\t",ptr[i]);
+	}
+	printf("\n\n");
+}
+
 int main(){
-	int arr[]={3,4,8,12},opt=0; 
+	int size=0,opt=0,pos=0,insert=0,add=0, end=0;
 	
-	printf("Origional\n");
-	  int size=sizeof(arr)/sizeof(arr[0]);
-	  for(int i=0;i<size;i++){
-		  printf("%d\t",arr[i]);
-	  }
-
-printf("which operation would you like to do?\n1.Add element in beginning\n2.Add element in the end\n3. Insert a element in between\n choose an option: ");
-scanf("%d",&opt);
+	// to add new element 
 	
-
-
-switch(opt){
-  case 3:	
-    
-    //to add a element in between
-    
-	 
-	  printf("\n");
-	  int pos=1;
-  	for(int i=size;i>=pos;i--){
-		  if(i==1){
-			  arr[2]=6;
-		  }else{
-			  arr[i+1]=arr[i];
-		  }
-	  }
-	  size+=1;
-	  display(arr,size);
-	  printf("\n");
-
-    break;
+	printf("How many numbers would you like to enter?");
+	scanf("%d",&size);
+	
+	int arr[size],org[size];
+	
+	for(int i=0;i<size;i++){
+		scanf("%d",&org[i]);
+	}
+	
+		
+	for(int i=0;i<size;i++){
+		arr[i]=org[i];                                            // duplicating the array
+	}
+	
+	printf("choose a option:\n1.add the element in the beginning\n2.add the element in the end\n3.insert element in the between\nwhat would you like to do? ");
+	scanf("%d",&opt);
 	
 	
-  case 1:
-    
-    //to add a element in the beginning of the array
-
-	  for(int i=size;i>=0;i--){
-		  arr[i+1]=arr[i];
-	  }
-	  arr[0]=10;
-	  size+=1;
-  	display(arr,size);
-
-    break;
+	switch(opt){
 	
+	case 3:                                                                                                 //to add a element in between
 
-  case 2:	
-    
-    //to add an element in the end
-
-	  arr[size]=64;
-	  size+=1;
-	  display(arr,size);
-
-    break;
-
-  default:
-    printf("Invalid option\n");
-
-    break;
-  }
+		printf("where you like to insert a number? ");
+		scanf("%d",&pos);
+		printf("enter a number: ");
+		scanf("%d",&insert);
+	
+		for(int i=size;i>=pos;i--){
+			if(i==(pos-1)){
+				arr[pos]=insert;
+			}else{
+				arr[i+1]=arr[i];
+			}
+		}
+		size+=1;
+		origional(org,size);
+		display(arr,size);
+		printf("\n\n");
+		
+		break;
+	
+	case 1:                                                                               //to add a element in the beginning of the array
+	
+		printf("enter a number: ");
+		scanf("%d",&add);
+	
+		for(int i=size;i>=0;i--){
+			arr[i+1]=arr[i];
+		}
+		arr[0]=add;
+		size+=1;
+		origional(org,size);
+		display(arr,size);
+		
+		break;
+	
+	case 2:                                           //to add an element in the end
+	
+		printf("enter a number: ");
+		scanf("%d",&end);
+	
+		arr[size]=end;
+		size+=1;
+		origional(org,size);
+		display(arr,size);
+		
+		break;
+		
+	default: 
+		printf("INVALID OPTION");
+		break;
+	}
 }
