c0110a23c48c56269ef97fc6da7bc1dc0694c758
Gareema Kumari Gupta <gareema.k.gupta@gmail.com>
2026-04-20 10:29:42 +0530

    Update array.c.c

--- array.c.c
+++ array.c.c
@@ -7,7 +7,7 @@ void display(int *ptr, int size) {
 }
 
 int main() {
-    int size, pos, insert,add,end;
+    int size, pos, insert,add,end,search,place=9999;
 
     printf("How many numbers would you like to enter? ");
     scanf("%d", &size);
@@ -21,66 +21,80 @@ int main() {
     printf("\nOriginal Array:\n");
     display(arr, size);
 
-        // Add at beginning
-            printf("\nEnter a number to insert at the beginning: ");
-            scanf("%d", &add);
+    // Add at beginning
+    printf("\nEnter a number to insert at the beginning: ");
+  	scanf("%d", &add);
 
-            for (int i = size - 1; i >= 0; i--) 
-                arr[i + 1] = arr[i];
+   	for (int i = size - 1; i >= 0; i--) 
+       arr[i + 1] = arr[i];
 
-            arr[0] = add;
-             printf("\nAdding element at the beginning:\n");
-            size++;
-            display(arr, size);
+	arr[0] = add;
+  	printf("\nAdding element at the beginning:\n");
+  	size++;
+   	display(arr, size);
 
-        // Add at end
-            printf("\nEnter a number to insert at the end: ");
-            scanf("%d", &end);
+    // Add at end
+    printf("\nEnter a number to insert at the end: ");
+    scanf("%d", &end);
 
-            arr[size] = end;
-            size++;
-            printf("\nAdding element at the end:\n");
-            display(arr, size);
+    arr[size] = end;
+    size++;
+    printf("\nAdding element at the end:\n");
+    display(arr, size);
 
-        // Insert in between
-            printf("\nEnter position (1 to %d): ", size + 1);
-            scanf("%d", &pos);
+    // Insert in between
+    printf("\nEnter position (1 to %d): ", size + 1);
+    scanf("%d", &pos);
 
-            if (pos < 1 || pos > size + 1) {
-                printf("Invalid position!\n");
-            }else{
+    if (pos < 1 || pos > size + 1) {
+         printf("Invalid position!\n");
+    }else{
 
-            printf("Enter a number to insert at the position %d: ",pos);
-            scanf("%d", &insert);
+    	printf("Enter a number to insert at the position %d: ",pos);
+    	scanf("%d", &insert);
 
-            pos = pos - 1;
+    	pos = pos - 1;
 
-            for (int i = size - 1; i >= pos; i--) 
-                arr[i + 1] = arr[i];
+    	for (int i = size - 1; i >= pos; i--) 
+         	arr[i + 1] = arr[i];
             
-            arr[pos] = insert;
-            size++;
-            printf("\nAdding a elemnt in between:\n");
-            display(arr, size);
-            }
+   		arr[pos] = insert;
+    	size++;
+    	printf("\nAdding a elemnt in between:\n");
+    	display(arr, size);
+    }
 
-        // deleting a element through its place value 
-            printf("Enter position to delete (1 to %d): ", size);
-            scanf("%d", &pos);
+    // deleting a element through its place value 
+    printf("Enter position to delete (1 to %d): ", size);
+    scanf("%d", &pos);
 
-            if (pos < 1 || pos > size) {
-                printf("Invalid position!\n");
-            }else{
+   	if (pos < 1 || pos > size) {
+         printf("Invalid position!\n");
+    }else{
 
-            pos = pos - 1;
+    	pos = pos - 1;
 
-            for (int i = pos; i < size - 1; i++) 
-                arr[i] = arr[i + 1];
+    	for (int i = pos; i < size - 1; i++) 
+         	arr[i] = arr[i + 1];
                 
-            size--;
-            printf("\nDeleting a element:\n");
-            display(arr, size);
-            }
+     	size--;
+     	printf("\nDeleting a element:\n");
+     	display(arr, size);
+     }
+     
+     //Searching an element
+     printf("Enter a number you would like to look for: ");
+     scanf("%d",&search);
+     
+     for(int i=0;i<size;i++){
+     	if(search==arr[i])
+     		place=i+1;
+     }
+     
+     if(place==9999)
+     	printf("%d element not found\n",search);
+     else
+     	printf("%d element found at position %d\n",search,place);
 
     return 0;
 }
