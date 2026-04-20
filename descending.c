44ac6adad83597c3dc063a334f3ebb67e1f2a903
Gareema Kumari Gupta <gareema.k.gupta@gmail.com>
2026-04-20 11:35:38 +0530

    Create descending.c

---
+++ descending.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+// Display updated array
+void display(int *ptr, int size) {
+    for (int i = 0; i < size; i++) 
+        printf("%d\t", ptr[i]);
+    printf("\n");
+}
+
+int main() {
+    int size;
+
+    printf("How many numbers would you like to enter? ");
+    scanf("%d", &size);
+
+    int arr[size],sort[size];
+
+    printf("Enter elements:\n");
+    for (int i = 0; i < size; i++) 
+        scanf("%d", &arr[i]);
+     	
+     // sorting
+     
+     printf("\nBefore Sorting:\n");
+     display(arr,size);
+     
+     for(int i = 1; i < size; i++) {
+    	for(int j = 1; j < size; j++) {
+        	if(arr[j-1] < arr[j]) {
+            	int t = arr[j-1];
+            	arr[j-1] = arr[j];
+            	arr[j] = t;
+       	 	}
+    	}
+	}
+     
+     printf("\nAfter Sorting it in Ascending order:\n");
+	display(arr,size);
+    return 0;
+}
